b3aa074390a8363630193f172ee57e7aadd9be93
Albert <Albert@HOME-PC>
2020-09-23 09:51:14 +0500

    [+] праметры отправки на UI [+] события по фокусам на текст боксах для анимации

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -24,5 +24,84 @@ namespace MailSender
         {
             InitializeComponent();
         }
+
+        #region обработчики событий по фокусу
+
+        //TODO: Переделать на триггеры и убрать обработчики из Code-Behind
+
+        private void TbFrom_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbFrom.Text == "Адрес отправителя" || tbFrom.Text == "")
+            {
+                tbFrom.Foreground = Brushes.Black;
+                tbFrom.Text = "";
+            }
+        }
+
+        private void TbFrom_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbFrom.Text == "Адрес отправителя" || tbFrom.Text == "")
+            {
+                tbFrom.Foreground = Brushes.LightGray;
+                tbFrom.Text = "Адрес отправителя";
+            }
+        }
+
+
+        #endregion
+
+        private void TbTo_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbTo.Text == "Адрес получателя" || tbTo.Text == "")
+            {
+                tbTo.Foreground = Brushes.Black;
+                tbTo.Text = "";
+            }
+        }
+
+        private void TbTo_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbTo.Text == "Адрес получателя" || tbTo.Text == "")
+            {
+                tbTo.Foreground = Brushes.LightGray;
+                tbTo.Text = "Адрес получателя";
+            }
+        }
+
+        private void TbSrvAddr_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbSrvAddr.Text == "Адрес сервера" || tbSrvAddr.Text == "")
+            {
+                tbSrvAddr.Foreground = Brushes.Black;
+                tbSrvAddr.Text = "";
+            }
+        }
+
+        private void TbSrvAddr_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbSrvAddr.Text == "Адрес сервера" || tbSrvAddr.Text == "")
+            {
+                tbSrvAddr.Foreground = Brushes.LightGray;
+                tbSrvAddr.Text = "Адрес сервера";
+            }
+        }
+
+        private void TbSrvPort_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbSrvPort.Text == "25" || tbSrvPort.Text == "")
+            {
+                tbSrvPort.Foreground = Brushes.Black;
+                tbSrvPort.Text = "";
+            }
+        }
+
+        private void TbSrvPort_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbSrvPort.Text == "25" || tbSrvPort.Text == "")
+            {
+                tbSrvPort.Foreground = Brushes.LightGray;
+                tbSrvPort.Text = "25";
+            }
+        }
     }
 }
