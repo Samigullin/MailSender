4bad8f51f3d5a4f604b48a3b2912b31dc983529a
Albert <Albert@HOME-PC>
2020-10-05 23:32:17 +0500

    [+]Валидация почты и имени получателя

--- MailSender/Models/Recipient.cs
+++ MailSender/Models/Recipient.cs
@@ -1,4 +1,6 @@
-﻿using MailSender.Models.Base;
+﻿using System;
+using System.Text.RegularExpressions;
+using MailSender.Models.Base;
 
 namespace MailSender.Models
 {
@@ -16,13 +18,30 @@ namespace MailSender.Models
         public string Name
         {
             get => _name;
-            set => Set(ref _name, value);
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Имя не может быть пустым!");
+                }
+
+                Set(ref _name, value);
+            } 
         }
 
         public string Address
         {
             get => _address;
-            set => Set(ref _address, value);
+            set
+            {
+                string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+                if (!Regex.IsMatch(value, pattern, RegexOptions.IgnoreCase))
+                {
+                    throw new ArgumentException("Не правильный формат почты!");
+                }
+                Set(ref _address, value);
+            }
         }
     }
 }
